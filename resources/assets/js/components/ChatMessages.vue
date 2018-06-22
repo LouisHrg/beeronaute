fd142b2de74e854a78214789fa9aabcdd59398f8
Sylvain Cabiati <cabiati.sylvain@outlook.com>
2018-06-22 14:42:45 +0200

    Debug user

--- resources/assets/js/components/ChatMessages.vue
+++ resources/assets/js/components/ChatMessages.vue
@@ -3,14 +3,19 @@
         <li class="left clearfix" v-for="message in messages">
             <div class="chat-body clearfix">
                 <div class="header">
-                    <a  v-bind:href="'/profile/' + message.user.name"><strong class="primary-font">
-                        {{ message.user.name }}
-                    </strong></a>
+                    <div v-if="!user">
+                        <a   v-bind:href="'/profile/' + message.user.name"><strong class="primary-font">
+                            {{ message.user.name }}
+                        </strong></a>
+                    </div>
+                    <div v-else>
+                        <strong>Anonyme</strong>
+                    </div>
                 </div>
                 <div v-if="user.name === message.user.name" style="background-color: #696969; color: white; border-radius: 5px;">
-                    <p style="padding: 10px; text-align: right;">
-                        {{ message.message }}
-                    </p>
+                        <p style="padding: 10px; text-align: right;">
+                            {{ message.message }}
+                        </p>
                 </div>
                 <div v-else style="background-color: #1b4f72; color: white ; border-radius: 5px;">
                     <p style="padding: 10px;">
